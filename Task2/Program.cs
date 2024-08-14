afb30bc449e927310789ce6bb24138300b545eeb
yasiia <malykyasya@gmail.com>
2024-08-14 21:56:38 +0300

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User! Enter the height of the triangle: ");
+            string userHeight = Console.ReadLine();
+            int height = int.Parse(userHeight);
+            for (int row = 0; row < height; row++)
+            {
+
+                for (int col = 0; col <= row; col++)
+                {
+                    Console.Write(row + 1);
+                }
+                Console.WriteLine();
+            }
+
+        }  
+    }
+ }
